01b7ea70e5de26ee8141a5c3c23720e1ec88e188
jayromberg <jayro.dev@gmail.com>
2023-07-01 23:08:41 -0300

    feat: update

--- scripts/preview.mjs
+++ scripts/preview.mjs
@@ -47,36 +47,3 @@ try {
 } finally {
   console.log('[COMMENT_ON_GITHUB: END]');
 }
-
-// ===================================
-// ===================================
-
-// deprecate code
-
-// const defaultHeaders = {};
-// defaultHeaders["authorization"] = `token ${GITHUB_TOKEN}`;
-// defaultHeaders["accept"] =
-//   "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
-// defaultHeaders["content-type"] = "application/json";
-
-// fetch(
-//   `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
-//   {
-//     headers: defaultHeaders,
-//     method: "POST",
-//     body: JSON.stringify({
-//       body: GH_COMMENT,
-//     }),
-//   }
-// )
-//   .then((response) => {
-//     if (response.ok) return response.json();
-//     throw new Error(response.statusText);
-//   })
-//   .catch((err) => {
-//     console.log("[COMMENT_ON_GITHUB: ERROR]");
-//     throw new Error(err);
-//   })
-//   .finally(() => {
-//     console.log("[COMMENT_ON_GITHUB: END]");
-//   });
